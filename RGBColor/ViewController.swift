9da8e180737895087a1345fd2c9aae6966c73432
Felix Titov <titow.feliks@yandex.ru>
2021-08-22 12:59:58 +0300

    Set constraints for paletteView and red slider

--- RGBColor/ViewController.swift
+++ RGBColor/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorPaletteLabel: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSlider.value = 0.05
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        paletteLabelLoad()
+    }
+    
+    
+    
+    private func paletteLabelLoad() {
+        colorPaletteLabel.layer.cornerRadius = 15
+        let startColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        colorPaletteLabel.backgroundColor = startColor
     }
 
-
+    @IBAction func changeRedValue() {
+        redValueLabel.text = String(format: "%.2f",redSlider.value)
+        
+    }
+    
+    
 }
 
