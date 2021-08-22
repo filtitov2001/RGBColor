7e89ed4771c5619c6d526f2a79444cd6529ab838
Felix Titov <titow.feliks@yandex.ru>
2021-08-22 16:32:56 +0300

    complete task

--- RGBColor/ViewController.swift
+++ RGBColor/ViewController.swift
@@ -12,30 +12,78 @@ class ViewController: UIViewController {
     @IBOutlet var colorPaletteLabel: UIView!
     
     @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
+
     @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redSlider.value = 0.05
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 1
+        redSliderLoad()
+        greenSliderLoad()
+        blueSliderLoad()
         paletteLabelLoad()
     }
     
-    
-    
-    private func paletteLabelLoad() {
-        colorPaletteLabel.layer.cornerRadius = 15
-        let startColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-        colorPaletteLabel.backgroundColor = startColor
-    }
 
     @IBAction func changeRedValue() {
-        redValueLabel.text = String(format: "%.2f",redSlider.value)
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        colorPaletteLabel.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
         
     }
     
     
+    @IBAction func changeGreenValue() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        colorPaletteLabel.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    
+    @IBAction func changeBlueValue() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        colorPaletteLabel.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
 }
 
+//MARK: Load main interface's elements
+extension ViewController {
+    private func redSliderLoad() {
+        redSlider.value = 0.05
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        redSlider.tintColor = .red
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    private func greenSliderLoad() {
+        greenSlider.value = 0.27
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        greenSlider.tintColor = .green
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        
+    }
+    
+    private func blueSliderLoad() {
+        blueSlider.value = 0.49
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        blueSlider.tintColor = .blue
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+    }
+    
+    private func paletteLabelLoad() {
+        colorPaletteLabel.layer.cornerRadius = 15
+        let startColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        colorPaletteLabel.backgroundColor = startColor
+    }
+    
+}
